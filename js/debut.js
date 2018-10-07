cf8981ca726e51a61f1c74bb05f407685726d711
Antoine Plagnol <32707818+plagnol@users.noreply.github.com>
2018-10-07 17:12:18 +0200

    third comit

--- js/debut.js
+++ js/debut.js
@@ -1,3 +1,5 @@
+//LANCEMENT DU SITE
+
 var lineDrawing = anime({
     targets: '#lineDrawing .lines path',
     strokeDashoffset: [anime.setDashoffset, 0],
@@ -34,11 +36,137 @@ var alternate = anime({
         }
     ],
     complete: function(){
-        alertee();
+        menu();
     }
 
 });
 
+function menuReset(){
 
+    $('#menuBase').html(" <div>\n" +
+        "        <div class=\"text-center\">\n" +
+        "            <div class=\"row\">\n" +
+        "                <div class=\"col-12 text-center\">\n" +
+        "                    <div class=\"rond rond-center\" id=\"rond1\" onclick=\"launchProjet()\">\n" +
+        "                        <p class=\"menuTitre\">PROJETS</p>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-5 text-right\">\n" +
+        "                    <div class=\"rond rond-left\" id=\"rond2\" onclick=\"launchExp()\">\n" +
+        "                        <p class=\"menuTitre\">EXPERIENCES</p>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-2 text-center\" id=\"centre\">\n" +
+        "                    MENU PRINCIPAL\n" +
+        "                </div>\n" +
+        "                <div class=\"col-5 text-left\">\n" +
+        "                    <div class=\"rond\" id=\"rond3\">\n" +
+        "                        <p class=\"menuTitre\">A PROPOS</p>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-12 text-center\">\n" +
+        "                    <div class=\"rond rond-center\" id=\"rond4\">\n" +
+        "                        <p class=\"menuTitre\">CONTACT</p>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>");
+}
+//MENU START
+function menu(){
+    $('#menuBase').fadeIn();
+}
+
+
+//START PROJECT
+function launchProjet(){
+    var cssProperties = anime({
+        targets: '#rond1',
+        opacity: .5,
+        backgroundColor: '#6D071A',
+        borderRadius: ['0%', '50%'],
+        easing: 'easeInOutQuad',
+        translateY: -800,
+        duration: 1000,
+        complete: function(){
+            $('#menuBase').css('display','none');
+            $('#projet').fadeIn();
+        }
+    });
+
+    var cssProperties = anime({
+        targets: '#rond2',
+        opacity: 0,
+        easing: 'easeInOutQuad',
+        translateX: 375,
+        duration: 1000
+    });
+    var cssProperties = anime({
+        targets: '#rond3',
+        opacity: 0,
+        easing: 'easeInOutQuad',
+        translateX: -375,
+        duration: 1000
+    });
+    var cssProperties = anime({
+        targets: '#rond4',
+        opacity: 0,
+        easing: 'easeInOutQuad',
+        translateY: -100,
+        duration: 1000
+    });
+}
+
+function retourMenu(){
+    var cssProperties = anime({
+        targets: '#rondProjet',
+        opacity: .5,
+        backgroundColor: '#6D071A',
+        borderRadius: ['0%', '50%'],
+        easing: 'easeInOutQuad',
+        translateY: 250,
+        duration: 500,
+        complete: function(){
+            $('#projet').css('display','none');
+            menuReset();
+            menu();
+        }
+    });
+}
+
+function launchExp(){
+    var cssProperties = anime({
+        targets: '#rond1',
+        opacity: 0,
+        easing: 'easeInOutQuad',
+        translateY: 100,
+        duration: 1000
+    });
+
+    var cssProperties = anime({
+        targets: '#rond2',
+        opacity: 0,
+        easing: 'easeInOutQuad',
+        backgroundColor: '#6D071A',
+        borderRadius: ['0%', '50%'],
+        translateX: -800,
+        duration: 1000
+    });
+    var cssProperties = anime({
+        targets: '#rond3',
+        opacity: 0,
+        easing: 'easeInOutQuad',
+        translateX: -375,
+        duration: 1000
+    });
+    var cssProperties = anime({
+        targets: '#rond4',
+        opacity: 0,
+        easing: 'easeInOutQuad',
+        translateY: -100,
+        duration: 1000
+    });
+}
 
 
